0ab88459ccec0f26003deba22c54707533135633
Chad Barraford <cbarraford@gmail.com>
2014-07-10 23:55:34 -0400

    add main lua

---
+++ main.lua
@@ -0,0 +1,39 @@
+-- Connect to Redis
+local redis = require "resty.redis"
+local red = redis:new()
+red:set_timeout(1000)
+local ok, err = red:connect("127.0.0.1", 6379)
+if not ok then
+    ngx.say("Failed to connect to Redis: ", err)
+    return
+end
+
+function string:split(delimiter)
+  local result = { }
+  local from = 1
+  local delim_from, delim_to = string.find( self, delimiter, from )
+  
+  while delim_from do
+    table.insert( result, string.sub( self, from , delim_from-1 ) )
+    from = delim_to + 1
+    delim_from, delim_to = string.find( self, delimiter, from )
+  end
+  
+  table.insert( result, string.sub( self, from ) )
+  
+  return result
+end
+
+app_name = ngx.var.uri:split('/')[2]
+ngx.log(ngx.ERR, app_name)
+
+
+account_name = ngx.var.host:split('%.')[1]
+ngx.log(ngx.ERR, account_name)
+
+-- get random upstream in key (aka name)
+local res, err = red:srandmember(account_name .. "_" .. app_name)
+if res then
+    ngx.log(ngx.ERR, res)
+    ngx.var.upstream = res
+end

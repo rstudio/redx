b6a7c17438b5e27102aa5c80be3501fa96ef0c59
Chad Barraford <cbarraford@gmail.com>
2014-07-18 15:43:27 -0400

    remove old commands from post-install script

--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 sudo apt-get install -y --force-yes vim curl
-rm /etc/nginx/nginx.conf
-test -f /etc/nginx/nginx.conf || sudo ln -s /home/vagrant/redx/nginx.conf /etc/nginx/nginx.conf
-sudo /bin/kill -HUP $(cat /run/nginx.pid) # reload nginx

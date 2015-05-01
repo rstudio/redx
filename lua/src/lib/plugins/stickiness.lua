b5b21ab432ed7850e35236026a7520c3d02e4777
Chad Barraford <cbarraford@gmail.com>
2015-05-01 14:04:00 -0400

    Stickiness should only look at the domain

--- lua/src/lib/plugins/stickiness.lua
+++ lua/src/lib/plugins/stickiness.lua
@@ -3,7 +3,7 @@ M.balance = function(request, session, param)
   local _list_0 = session['servers']
   for _index_0 = 1, #_list_0 do
     local server = _list_0[_index_0]
-    if request.session.backend == server['address'] then
+    if request.session.backend == M.extract_domain(server['address']) then
       return server
     end
   end
@@ -11,7 +11,16 @@ M.balance = function(request, session, param)
 end
 M.post = function(request, session, param)
   if session['server'] then
-    request.session.backend = session['server']
+    request.session.backend = M.extract_domain(session['server'])
+  end
+end
+M.extract_domain = function(url)
+  if not string.match(url, '/') then
+    return url
+  else
+    local location_index = string.find(url, '/')
+    local domain = string.sub(url, 1, (location_index - 1))
+    return domain
   end
 end
 return M
